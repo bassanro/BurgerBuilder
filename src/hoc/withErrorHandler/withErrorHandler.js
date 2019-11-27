1bd21956a976d68abac8abc135c5f8bc1fa55a6f
Roshan Bassan <roshan.bassan@gmai.com>
2019-11-27 14:42:58 +1100

    Remove old interceptor and verify

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,16 +10,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
     constructor(props) {
       super(props);
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
         }
       );
+    }
+
+    // return function of UseEffect.
+    componentWillUnmount() {
+      console.log("Will unmount", this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
