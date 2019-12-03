345f61596c6ae2d131d5048fedc8a4310137eb7e
ROSHAN BASSAN <roshan.bassan@gmail.com>
2019-12-04 00:30:25 +1100

    Fix for price and other minor fixes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" exact component={Orders} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -28,11 +28,11 @@ class Checkout extends Component {
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  };
+  }
 
   checkoutContinuedHandler = () => {
     this.props.history.replace("/checkout/contact-data");
-  };
+  }
 
   render() {
     return (
